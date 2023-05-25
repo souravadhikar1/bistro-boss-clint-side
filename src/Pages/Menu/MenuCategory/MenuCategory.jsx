986a9e9d9ba2e4a2fa46b75dab25996e0f984bad
souravadhikar1 <souravadhikari09296@gmail.com>
2023-05-25 11:23:01 +0600

    local to server connected

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 import Cover from "../../Shared/Cover/Cover";
+import { Link } from "react-router-dom";
 
 const MenuCategory = ({ items, title, img }) => {
   return (
@@ -12,11 +13,11 @@ const MenuCategory = ({ items, title, img }) => {
             <MenuItem key={item._id} item={item}></MenuItem>
           ))}
         </div>
-        {/* <Link to={`/order/${title}`}>
-            <button className="btn btn-outline border-0 border-b-4 mt-4">
-              Order Now
-            </button>
-          </Link> */}
+        <Link to={`/order/${title}`}>
+          <button className="btn btn-outline border-0 border-b-4 mt-4">
+            Order Now
+          </button>
+        </Link>
       </div>
     </div>
   );
